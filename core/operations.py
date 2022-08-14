a6a3a19390319ac99f2987a0d4781d4b6ea1f043
Resul Emre AYGAN <remreaygan@gmail.com>
2022-08-14 15:21:24 +0300

    added support for array transpose

--- core/operations.py
+++ core/operations.py
@@ -6,7 +6,19 @@ Version: 0.0.1
 E-mail: remreaygan@gmail.com
 """
 
+import numpy as np
+
 
 class Operations:
     def __init__(self):
         pass
+
+    @staticmethod
+    def transpose_array(array, order=(1, 2, 0)):
+        """
+        Transposes the numpy array according to the desired order.
+        :param array: ndarray as shape (x, y, z)
+        :param order: transpose order
+        :return: transposed array
+        """
+        return np.transpose(array, order)
