860fe43f98d7a7dc7a8b6ca5776a27b651bc3973
Resul Emre AYGAN <remreaygan@gmail.com>
2022-08-14 15:30:11 +0300

    added support for rgb to bgr transformation

--- core/operations.py
+++ core/operations.py
@@ -5,7 +5,7 @@ License: MIT
 Version: 0.0.1
 E-mail: remreaygan@gmail.com
 """
-
+import cv2
 import numpy as np
 
 
@@ -22,3 +22,12 @@ class Operations:
         :return: transposed array
         """
         return np.transpose(array, order)
+
+    @staticmethod
+    def rgb_to_bgr(array):
+        """
+        Converts the image from rgb to bgr with OpenCV.
+        :param array: input image as ndarray
+        :return: bgr image as ndarray
+        """
+        return cv2.cvtColor(array.astype(np.float32), cv2.COLOR_RGB2BGR)
